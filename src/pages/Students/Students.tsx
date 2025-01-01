7cb0bed2e2eaef0cad71cc85b5d8b0594ff63013
unknown <dtai858@gmail.com>
2025-01-01 16:50:05 +0700

    convert method to tanstack query for optimizing

--- src/pages/Students/Students.tsx
+++ src/pages/Students/Students.tsx
@@ -1,19 +1,28 @@
+import { useQuery } from '@tanstack/react-query'
 import { getStudents } from 'apis/students.api'
-import { useEffect, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Student, Students as studentType } from 'types/students.type'
+import { useSearchParams } from 'react-router-dom'
 
 export default function Students() {
-  const [students, setStudents] = useState<studentType>([])
-  const [isLoading, setIsLoading] = useState<boolean>(false)
+  // const [students, setStudents] = useState<studentType>([])
+  // const [isLoading, setIsLoading] = useState<boolean>(false)
 
-  useEffect(() => {
-    setIsLoading(true)
-    getStudents(1, 10).then((res) => {
-      setStudents(res.data)
-    }).finally(() => setIsLoading(false))
-  }, [])
-
+  // useEffect(() => {
+  //   setIsLoading(true)
+  //   getStudents(1, 10).then((res) => {
+  //     setStudents(res.data)
+  //   }).finally(() => setIsLoading(false))
+  // }, [])
+  const [seachParams] = useSearchParams()
+  const searchParamsObject = Object.fromEntries([...seachParams])
+  console.log(searchParamsObject)
+  // const result = useQuery({
+  //   queryKey: ['students', page],
+  //   queryFn: () => getStudents(page , 10)
+  // })
+  
   return (
     <div>
       <h1 className='text-lg'>Students</h1>
@@ -33,55 +42,55 @@ export default function Students() {
         <div className='h-10  rounded bg-gray-200 dark:bg-gray-700' />
         <span className='sr-only'>Loading...</span>
       </div>
-      : <div className='relative mt-6 overflow-x-auto shadow-md sm:rounded-lg'>
-        <table className='w-full text-left text-sm text-gray-500 dark:text-gray-400'>
-          <thead className='bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400'>
-            <tr>
-              <th scope='col' className='py-3 px-6'>
-                #
-              </th>
-              <th scope='col' className='py-3 px-6'>
-                Avatar
-              </th>
-              <th scope='col' className='py-3 px-6'>
-                Name
-              </th>
-              <th scope='col' className='py-3 px-6'>
-                Email
-              </th>
-              <th scope='col' className='py-3 px-6'>
-                <span className='sr-only'>Action</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {students.map((student, index) => (
-              <tr key={index} className='border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600'>
-              <td className='py-4 px-6'>{index}</td>
-              <td className='py-4 px-6'>
-                <img
-                  src={student.avatar}
-                  alt='student'
-                  className='h-5 w-5'
-                />
-                </td>
-                <th scope='row' className='whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white'>
-                  {student.last_name}
+      : <Fragment>
+      :   <div className='relative mt-6 overflow-x-auto shadow-md sm:rounded-lg'>
+          <table className='w-full text-left text-sm text-gray-500 dark:text-gray-400'>
+            <thead className='bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400'>
+              <tr>
+                <th scope='col' className='py-3 px-6'>
+                  #
                 </th>
-                <td className='py-4 px-6'>{student.email}</td>
-                <td className='py-4 px-6 text-right'>
-                  <Link to='/students/1' className='mr-5 font-medium text-blue-600 hover:underline dark:text-blue-500'>
-                    Edit
-                  </Link>
-                  <button className='font-medium text-red-600 dark:text-red-500'>Delete</button>
-                </td>
+                <th scope='col' className='py-3 px-6'>
+                  Avatar
+                </th>
+                <th scope='col' className='py-3 px-6'>
+                  Name
+                </th>
+                <th scope='col' className='py-3 px-6'>
+                  Email
+                </th>
+                <th scope='col' className='py-3 px-6'>
+                  <span className='sr-only'>Action</span>
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>}
-
-      <div className='mt-6 flex justify-center'>
+            </thead>
+            <tbody>
+              {students.map((student, index) => (
+                <tr key={index} className='border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600'>
+                <td className='py-4 px-6'>{index}</td>
+                <td className='py-4 px-6'>
+                  <img
+                    src={student.avatar}
+                    alt='student'
+                    className='h-5 w-5'
+                  />
+                  </td>
+                  <th scope='row' className='whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white'>
+                    {student.last_name}
+                  </th>
+                  <td className='py-4 px-6'>{student.email}</td>
+                  <td className='py-4 px-6 text-right'>
+                    <Link to='/students/1' className='mr-5 font-medium text-blue-600 hover:underline dark:text-blue-500'>
+                      Edit
+                    </Link>
+                    <button className='font-medium text-red-600 dark:text-red-500'>Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className='mt-6 flex justify-center'>
         <nav aria-label='Page navigation example'>
           <ul className='inline-flex -space-x-px'>
             <li>
@@ -108,6 +117,7 @@ export default function Students() {
           </ul>
         </nav>
       </div>
+      : </Fragment>}
     </div>
   )
 }
