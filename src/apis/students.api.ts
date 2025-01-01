c7b1b74842a28c50093fbe711273435f0effe133
unknown <dtai858@gmail.com>
2025-01-01 16:17:03 +0700

    fetch data from fake api server and adjust logic for students data

--- src/apis/students.api.ts
+++ src/apis/students.api.ts
@@ -1,11 +1,10 @@
 import { Students } from "types/students.type"
 import http from "utils/http"
 
-export const getStudents = (_page: number | string, _limit: number | string) => {
+export const getStudents = (_page: number | string, _limit: number | string) => 
   http.get<Students>('students', {
     params: {
       _page,
       _limit
     }
   })
-}
