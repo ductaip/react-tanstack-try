22391226d44eca4d8846bbe1853a6314d8305373
unknown <dtai858@gmail.com>
2025-01-01 15:33:45 +0700

    setup http service and type res axios

--- src/apis/students.api.ts
+++ src/apis/students.api.ts
@@ -1,7 +1,8 @@
+import { Students } from "types/students.type"
 import http from "utils/http"
 
 export const getStudents = (_page: number | string, _limit: number | string) => {
-  http.get('students', {
+  http.get<Students>('students', {
     params: {
       _page,
       _limit
